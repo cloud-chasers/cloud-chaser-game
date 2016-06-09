d03d9c0c907280d052c7bf5570a376f0ed904e80
mkv-manual <zfroeber@gmail.com>
2016-06-08 20:24:01 -0700

    6/7 Zach work at home

--- app/src/main/java/com/example/froezac17/myapplication/Arrow.java
+++ app/src/main/java/com/example/froezac17/myapplication/Arrow.java
@@ -1,7 +1,21 @@
 package com.example.froezac17.myapplication;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by Zach Froeber on 6/8/2016.
  */
-public class Arrow {
+public class Arrow extends GameObject {
+
+    private Bitmap img;
+
+    public Arrow(Bitmap res, int x, int y){
+
+        this.Ix = x;
+        this.Iy = y;
+        img = res;
+
+    }
+
+
 }
